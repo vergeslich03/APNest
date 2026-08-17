2d8d10f446417fee3324f50a925ffe5ffe3e87b5
vergeslich03 <vergeslichlp@gmail.com>
2026-08-17 12:51:23 +0200

    name change from previous commit

--- worlds/iron_nest/Regions.py
+++ worlds/iron_nest/Regions.py
@@ -1,10 +1,10 @@
 from BaseClasses import MultiWorld, Region
-from .Locations import INLocation, medal_ids, mission_ids
+from .Locations import INLocation, medal_locations, mission_locations
 
 def mission_regions(world: MultiWorld, player: int):
     world.regions.append(Region("Menu", player, world))
 
-    for mission_name, mission_id in mission_ids.items():
+    for mission_name, mission_id in mission_locations.items():
         region = Region(mission_name, player, world)
         region.locations += [
             INLocation(player, mission_name, mission_id, region)
@@ -12,11 +12,11 @@ def mission_regions(world: MultiWorld, player: int):
         world.regions.append(region)
 
 def medal_regions(world: MultiWorld, player: int):
-    for mission_name in mission_ids.keys():
+    for mission_name in mission_locations.keys():
         region = world.get_region(mission_name, player)
         region.locations += [
             INLocation(player, medal_name, medal_id, region)
             for medal_name, medal_id
-            in medal_ids.items()
+            in medal_locations.items()
             if medal_name.startswith(mission_name)
         ]
