6d97df0419121fe6e325a98925d6a06c209375b9
vergeslich03 <vergeslichlp@gmail.com>
2026-09-01 22:04:26 +0200

    reorganized items to combat the location to item deficit

--- worlds/iron_nest/Items.py
+++ worlds/iron_nest/Items.py
@@ -10,28 +10,13 @@ class INItem(Item):
 # YYY --> Item
 
 progression_items = {
-    "Punchcard - HE Shell": 9140001,
-    "Punchcard - STAR Shell": 9140002,
-    "Punchcard - AP Shell": 9140003,
-    "Punchcard - SMK Shell": 9140004,
-    "Punchcard - TEAR Shell": 9140005,
-    "Punchcard - INCN Shell": 9140006,
-    "Punchcard - HCHE Shell": 9140007,
-    "Punchcard - DRIL Shell": 9140008,
-    "Punchcard - LE Shell": 9140009,
-    "Punchcard - PHGN Shell": 9140010,
-    "Punchcard - WP Shell": 9140011,
-    "Punchcard - PCLM Shell": 9140012,
-    "Punchcard - APHE Shell": 9140013,
-    "Punchcard - FLCH Shell": 9140014,
-    "Punchcard - PRPG Shell": 9140015,
-    "Punchcard - THRM Shell": 9140016,
-    "Punchcard - ATMC Shell": 9140017,
-    "Punchcard - CYAN Shell": 9140018,
-    "Punchcard - EQKE Shell": 9140019,
-    "Punchcard - Powder Charges": 9140020,
-    "Punchcard - Scout Plane": 9140021,
-    "Punchcard - Emergency Move": 9140022,
+    "Punchcard - AP Shell": 9140001,
+    "Punchcard - SMK Shell": 9140002,
+    "Punchcard - TEAR Shell": 9140003,
+    "Punchcard - ATMC Shell": 9140004,
+    "Punchcard - Powder Charges": 9140005,
+    "Punchcard - Scout Plane": 9140006,
+    "Punchcard - Emergency Move": 9140007,
 }
 
 utility_items = {
@@ -39,6 +24,21 @@ utility_items = {
     "Requisition - Spotter": 9141002,
     "Punchcard - Location Report": 9141003,
     "Requisition - Location Report": 9141004,
+    "Punchcard - HE Shell": 9141005,
+    "Punchcard - STAR Shell": 9141006,
+    "Punchcard - INCN Shell": 9141007,
+    "Punchcard - HCHE Shell": 9141008,
+    "Punchcard - DRIL Shell": 9141009,
+    "Punchcard - LE Shell": 9141010,
+    "Punchcard - PHGN Shell": 9141011,
+    "Punchcard - WP Shell": 9141012,
+    "Punchcard - PCLM Shell": 9141013,
+    "Punchcard - APHE Shell": 9141014,
+    "Punchcard - FLCH Shell": 9141015,
+    "Punchcard - PRPG Shell": 9141016,
+    "Punchcard - THRM Shell": 9141017,
+    "Punchcard - CYAN Shell": 9141018,
+    "Punchcard - EQKE Shell": 9141019,
 }
 
 filler_items = {
@@ -52,6 +52,15 @@ trap_items = {
     "Trap - Sabotage": 9143003,
     "Trap - Counter-Battery": 9143004,
 }
+
+support_items = {
+    "Punchcard - Spotter",
+    "Requisition - Spotter",
+    "Punchcard - Location Report",
+    "Requisition - Location Report",
+}
+
+demoted_cards = set(utility_items) - support_items
 
 all_items = {
     **progression_items,
